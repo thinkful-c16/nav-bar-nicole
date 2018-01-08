6f06a5f75c09162859cf058a85770aa78380c669
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-08 13:07:33 -0800

    initial jquery functinality added

---
+++ index.js
@@ -0,0 +1,9 @@
+$("#course-link").on('click', function (e) {
+    $('#course-nav').toggleClass("hidden"); 
+    e.preventDefault();
+})
+
+$("#how-link").on('click', function (e) {
+    $('#how-nav').toggleClass("hidden"); 
+    e.preventDefault();
+})
